72c1708ac581e68dcf5efd8035c400b90acc4e55
Rafael Pianaro <rafaelpianaro@gmail.com>
2024-04-15 21:58:09 -0300

    feat: creating chirps

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -33,6 +33,9 @@ new class extends Component
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('chirps')" :active="request()->routeIs('chirps')" wire:navigate>
+                        {{ __('Chirps') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -85,6 +88,9 @@ new class extends Component
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+        <x-responsive-nav-link :href="route('chirps')" :active="request()->routeIs('chirps')" wire:navigate>
+            {{ __('Chirps') }}
+        </x-responsive-nav-link>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
